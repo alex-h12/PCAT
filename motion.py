39a4f9b2571c6642607aa3f3889cdeb8bde58d98
alex-h12 <alex.hays@doverfs.com>
2026-07-08 11:18:40 -0500

    Update motion.py

--- motion.py
+++ motion.py
@@ -1,11 +1,13 @@
 import time
 
+# Calculated conversion values between GRBL units and millimeters. Don't change unless you're changing units
 GRBL_SCALE_X = 50.0
 GRBL_SCALE_Y = 50.0
 GRBL_SCALE_Z = 1.0
 
-def get_position(grbl): #Converts GRBL position to real world mm
-    position = grbl.get_position()
+#Converts GRBL position to real world mm
+def get_position(grbl): 
+    position = grbl.get_position()       
 
     if position is None:
         raise RuntimeError("Could not read GRBL position.")
@@ -20,24 +22,26 @@ def get_position(grbl): #Converts GRBL position to real world mm
         "z": z
     }
 
-def move(grbl, x=None, y=None, z=None, feedrate = 100): #Converts real world mm measurements to managable GRBL steps
+#Converts real world mm measurements back to GRBL units
+def move(grbl, x=None, y=None, z=None, feedrate = 100): 
     if x is not None:
-        x = x / 50
+        x = x / GRBL_SCALE_X
     else:
         x = None
 
     if y is not None:
-        y = y / 50
+        y = y / GRBL_SCALE_Y
     else:
         y = None
 
     if z is not None:
-        z = z
+        z = z / GRBL_SCALE_Z
     else:
         z = None
 
     grbl.move_to(x, y, z, feedrate)
 
+#Used to jog the motors via keyboard inputs (keyPressEvent function in touch_logger.py)
 def jog(grbl, dx=0.0, dy=0.0, dz=0.0, feedrate=500):
     position = get_position(grbl)
 
@@ -49,16 +53,24 @@ def jog(grbl, dx=0.0, dy=0.0, dz=0.0, feedrate=500):
         feedrate = feedrate
     )
 
-def approach(x, y, safe_z, xy_feedrate, z_feedrate): #Moves above a point without unnecessary collision
-    position = get_position()
+#Moves above an xy point at a safe z position
+def approach(grbl, x, y, safe_z, xy_feedrate, z_feedrate): 
+    move(
+        grbl,
+        z = safe_z, 
+        feedrate = z_feedrate
+    )
 
-    if position["z"] < safe_z:
-        move(z=safe_z, feedrate=z_feedrate)
+    move(
+        grbl,
+        x = x, 
+        y = y, 
+        feedrate = xy_feedrate
+    )
 
-    move(x=x, y=y, feedrate=xy_feedrate)
-
-def press_until_force(force_logger, target_force, z_step, z_feedrate): #Moves down until target force is reached
-        position = get_position()
+#Moves z axis down until target force is reached
+def press_until_force(grbl, force_logger, target_force, z_step, z_feedrate): 
+        position = get_position(grbl)
         z = position["z"]
 
         while True:
@@ -68,4 +80,9 @@ def press_until_force(force_logger, target_force, z_step, z_feedrate): #Moves do
                 return
 
             z += z_step
-            move(z=z, feedrate=z_feedrate)
+
+            move(
+                grbl,
+                z = z, 
+                feedrate = z_feedrate
+            )
